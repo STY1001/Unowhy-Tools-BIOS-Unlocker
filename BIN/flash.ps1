2e57f83a1703de2b61ece6e5aa7d8bd0fe3faf69
STY1001 <57889852+STY1001@users.noreply.github.com>
2025-09-12 09:43:11 +0200

    Rewrite and preparation for 2025

--- BIN/flash.ps1
+++ BIN/flash.ps1
@@ -1,6 +1,8 @@
 # Unowhy Tools BIOS Unlocker
 # by STY1001
 
+#
+
 function header {
     Write-Host "Unowhy Tools BIOS Unlocker"
     Write-Host "for Unowhy Y13"
@@ -16,9 +18,11 @@ function confver {
     Write-Host "[1] 2019"
     Write-Host "[2] 2020"
     Write-Host "[3] 2021"
-    Write-Host "[4] 2022"
-    Write-Host "[5] 2022 (Alt)"
-    Write-Host "[7] 2024"
+    Write-Host "[4] 2022 (0.1.1)"
+    Write-Host "[5] 2022 (0.5.1)"
+    Write-Host "[6] 2023 (0.20.15)"
+    Write-Host "[7] 2024 (0.20.11)"
+    Write-Host "[8] 2025"
     $confver = Read-Host
     if ($confver -eq '1') {
         $pcversion = "2019"
@@ -30,19 +34,54 @@ function confver {
         $pcversion = "2021"
     }
     if ($confver -eq '4') {
-        $pcversion = "2022"
+        $pcversion = "2022_1"
     }
     if ($confver -eq '5') {
-        $pcversion = "2022alt"
+        $pcversion = "2022_2"
     }
     if ($confver -eq '6') {
-        $pcversion = "2023"
+        $pcversion = "2023_1"
     }
     if ($confver -eq '7') {
-        $pcversion = "2024"
+        $pcversion = "2024_1"
+    }
+    if ($confver -eq '8') {
+        $pcversion = "2025_1"
     }
 
     return $pcversion
+}
+
+function model2label ([string]$model) {
+
+    $modellabel = ""
+
+    if ($model.contains("2019")) {
+        $modellabel = "Unowhy Y13 2019"
+    }
+    if ($model.contains("2020")) {
+        $modellabel = "Unowhy Y13 2020"
+    }
+    if ($model.contains("2021")) {
+        $modellabel = "Unowhy Y13 2021"
+    }
+    if ($model.contains("2022_1")) {
+        $modellabel = "Unowhy Y13 2022 (0.1.1)"
+    }
+    if ($model.contains("2022_2")) {
+        $modellabel = "Unowhy Y13 2022 (0.5.1)"
+    }
+    if ($model.contains("2023_1")) {
+        $modellabel = "Unowhy Y13 2023 (0.20.15)"
+    }
+    if ($model.contains("2024_1")) {
+        $modellabel = "Unowhy Y13 2024 (0.20.11)"
+    }
+    if ($model.contains("2025_1")) {
+        $modellabel = "Unowhy Y13 2025"
+    }
+
+    return $modellabel
 }
 
 function binselect ([string]$pcversion) {
@@ -52,10 +91,11 @@ function binselect ([string]$pcversion) {
     $bin2019 = "BIOS_2019_NoPwd.rom"
     $bin2020 = "BIOS_2020_NoPwd.rom"
     $bin2021 = "BIOS_2021_NoPwd.rom"
-    $bin2022 = "Y13_2022_Unlocked.rom"
-    $bin2022alt = "Y13_2022_Unlocked_Alt.rom"
-    $bin2023 = "Y13_Software_2023_Unlocked.bin"
-    $bin2024 = "Y13_Software_2024_Unlocked.bin"
+    $bin2022_1 = "Y13_2022_Unlocked_0.1.1.rom"
+    $bin2022_2 = "Y13_2022_Unlocked_0.5.1.rom"
+    $bin2023_1 = "Y13_Software_2023_0.20.15_Unlocked.bin"
+    $bin2024_1 = "Y13_Software_2024_0.20.11_Unlocked.bin"
+    $bin2025_1 = "Y13_Software_2025_Unlocked.bin"
 
     if ($pcversion.contains("2019")) {
         $binreturn = $bin2019
@@ -66,17 +106,20 @@ function binselect ([string]$pcversion) {
     if ($pcversion.contains("2021")) {
         $binreturn = $bin2021
     }
-    if ($pcversion.contains("2022")) {
-        $binreturn = $bin2022
+    if ($pcversion.contains("2022_1")) {
+        $binreturn = $bin2022_1
     }
-    if ($pcversion.contains("2022alt")) {
-        $binreturn = $bin2022alt
+    if ($pcversion.contains("2022_2")) {
+        $binreturn = $bin2022_2
     }
-    if ($pcversion.contains("2023")) {
-        $binreturn = $bin2023
+    if ($pcversion.contains("2023_1")) {
+        $binreturn = $bin2023_1
     }
-    if ($pcversion.contains("2024")) {
-        $binreturn = $bin2024
+    if ($pcversion.contains("2024_1")) {
+        $binreturn = $bin2024_1
+    }
+    if ($pcversion.contains("2025_1")) {
+        $binreturn = $bin2025_1
     }
 
     return $binreturn
@@ -84,7 +127,7 @@ function binselect ([string]$pcversion) {
 
 function flash ([string]$pcversion, [string]$binpathfinal) {
 
-    if ($pcversion.Contains("2023") -or $pcversion.Contains("2024")) {
+    if ($pcversion.Contains("2023") -or $pcversion.Contains("2024") -or $pcversion.Contains("2025")) {
         .\FPTW.exe -BIOS -F $binpathfinal
     }
     else {
@@ -111,24 +154,39 @@ if ($model.contains("Y13G011S4EI")) {
     $pcversion = "2021"
 }
 if ($model.contains("Y13G012S4EI")) {
+    if($biosver.contains("0.1.1")) {
+        $pcversion = "2022_1"
+    }
     if($biosver.contains("0.5.1")) {
-        $pcversion = "2022alt"
+        $pcversion = "2022_2"
     }
-    else {
-        $pcversion = "2022"
-    }
+    $pcversion = "2022_1"
 }
 if ($model.contains("Y13G113S4EI")) {
-    $pcversion = "2023"
+    if($biosver.contains("0.20.15")) {
+        $pcversion = "2023_1"
+    }
+    $pcversion = "2023_1"
 }
 if ($model.contains("Y13G201S4EI")) {
-    $pcversion = "2024"
+    if($biosver.contains("0.20.11")) {
+        $pcversion = "2024_1"
+    }
+    $pcversion = "2024_1"
+}
+if ($model.contains("Y13G202S4EI"))
+{
+    $pcversion = "2025"
 }
 
 clear
 header
 
 if ($pcversion.contains("null")) {
+    Write-Host "This PC: (Save this info in case of problem)"
+    Write-Host "- SKU: $($model)"
+    Write-Host "- BIOS Version: $($biosver)"
+    Write-Host ""
     Write-Host "Are you sure that PC is an Unowhy Y13 ?"
     $confY13 = Read-Host "[Y]/[N]"
     Write-Host ""
@@ -138,7 +196,11 @@ if ($pcversion.contains("null")) {
     }
 }
 else {
-    Write-Host "Are you sure that Unowhy Y13 is an"$pcversion" ?"
+    Write-Host "This PC: (Save this info in case of problem)"
+    Write-Host "- SKU: $($model)"
+    Write-Host "- BIOS Version: $($biosver)"
+    Write-Host ""
+    Write-Host "Are you sure that Unowhy Y13 is a $(model2label($pcversion)) ?"
     $confpcver = Read-Host "[Y]/[N]"
     Write-Host ""
     if ($confpcver -eq 'n') {
@@ -157,7 +219,7 @@ clear
 header
 
 if ($isY13.Contains("true")) {
-    Write-Host "You are ready to flash (Unowhy Y13" $pcversion ")"
+    Write-Host "You are ready to flash (Unowhy Y13 $(model2label($pcversion)))"
     Write-Host "Do you want to proceed now ?" -ForegroundColor Green
     Write-Host "Warning: Please, put PLUG your charger, CLOSE your programs, SAVE your works and DON'T CLOSE this window OR SHUTDOWN your PC, until the flash stop !!!" -ForegroundColor Red
     $conf = Read-Host "[Y]/[N]"
@@ -180,5 +242,6 @@ if ($flashproceed.Contains("true")) {
     flash $pcversion $binfinal
     Write-Host ""
     Write-Host "Please Reboot, To access to the BIOS, press DEL or ESC at boot"
+    pause
     exit
 }

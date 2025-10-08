31358763cfdf66af3e4587c1adf486ffb77423b0
STY1001 <57889852+STY1001@users.noreply.github.com>
2025-10-08 17:26:43 +0200

    Optimizing the code with switch case and fix a bug with the selection

--- BIN/flash.ps1
+++ BIN/flash.ps1
@@ -1,17 +1,22 @@
 # Unowhy Tools BIOS Unlocker
 # by STY1001
 
-#
+[string]$model = Get-CimInstance -Classname Win32_ComputerSystem | Select-Object SystemSKUNumber
+[string]$biosver = Get-CimInstance -Classname Win32_BIOS | Select-Object SMBIOSBIOSVersion
 
 function header {
     Write-Host "Unowhy Tools BIOS Unlocker"
     Write-Host "for Unowhy Y13"
     Write-Host "by STY1001"
     Write-Host ""
+    Write-Host "This PC: (Save this info in case of problem)"
+    Write-Host "- SKU: $($model)"
+    Write-Host "- BIOS Version: $($biosver)"
+    Write-Host ""
 }
 
 function confver {
-    clear
+    Clear-Host
     header
 
     Write-Host "Select the version of this Unowhy Y13"
@@ -23,30 +28,19 @@ function confver {
     Write-Host "[6] 2023 (0.20.15)"
     Write-Host "[7] 2024 (0.20.11)"
     Write-Host "[8] 2025 (0.20.18)"
-    $confver = Read-Host
-    if ($confver -eq '1') {
-        $pcversion = "2019"
-    }
-    if ($confver -eq '2') {
-        $pcversion = "2020"
-    }
-    if ($confver -eq '3') {
-        $pcversion = "2021"
-    }
-    if ($confver -eq '4') {
-        $pcversion = "2022_1"
-    }
-    if ($confver -eq '5') {
-        $pcversion = "2022_2"
-    }
-    if ($confver -eq '6') {
-        $pcversion = "2023_1"
-    }
-    if ($confver -eq '7') {
-        $pcversion = "2024_1"
-    }
-    if ($confver -eq '8') {
-        $pcversion = "2025_1"
+    $confver = Read-Host "[1-8]:"
+    switch ($confver) {
+        '1' { $pcversion = "2019" }
+        '2' { $pcversion = "2020" }
+        '3' { $pcversion = "2021" }
+        '4' { $pcversion = "2022_1" }
+        '5' { $pcversion = "2022_2" }
+        '6' { $pcversion = "2023_1" }
+        '7' { $pcversion = "2024_1" }
+        '8' { $pcversion = "2025_1" }
+        default { 
+            $pcversion = confver
+        }
     }
 
     return $pcversion
@@ -56,29 +50,16 @@ function model2label ([string]$model) {
 
     $modellabel = ""
 
-    if ($model.contains("2019")) {
-        $modellabel = "2019"
-    }
-    if ($model.contains("2020")) {
-        $modellabel = "2020"
-    }
-    if ($model.contains("2021")) {
-        $modellabel = "2021"
-    }
-    if ($model.contains("2022_1")) {
-        $modellabel = "2022 (0.1.1)"
-    }
-    if ($model.contains("2022_2")) {
-        $modellabel = "2022 (0.5.1)"
-    }
-    if ($model.contains("2023_1")) {
-        $modellabel = "2023 (0.20.15)"
-    }
-    if ($model.contains("2024_1")) {
-        $modellabel = "2024 (0.20.11)"
-    }
-    if ($model.contains("2025_1")) {
-        $modellabel = "2025 (0.20.18)"
+    switch -Regex ($model) {
+        "2019" { $modellabel = "2019" }
+        "2020" { $modellabel = "2020" }
+        "2021" { $modellabel = "2021" }
+        "2022_1" { $modellabel = "2022 (0.1.1)" }
+        "2022_2" { $modellabel = "2022 (0.5.1)" }
+        "2023_1" { $modellabel = "2023 (0.20.15)" }
+        "2024_1" { $modellabel = "2024 (0.20.11)" }
+        "2025_1" { $modellabel = "2025 (0.20.18)" }
+        default { $modellabel = "" }
     }
 
     return $modellabel
@@ -97,29 +78,16 @@ function binselect ([string]$pcversion) {
     $bin2024_1 = "Y13_Software_2024_0.20.11_Unlocked.bin"
     $bin2025_1 = "Y13_Software_2025_0.20.18_Unlocked.bin"
 
-    if ($pcversion.contains("2019")) {
-        $binreturn = $bin2019
-    }
-    if ($pcversion.contains("2020")) {
-        $binreturn = $bin2020
-    }
-    if ($pcversion.contains("2021")) {
-        $binreturn = $bin2021
-    }
-    if ($pcversion.contains("2022_1")) {
-        $binreturn = $bin2022_1
-    }
-    if ($pcversion.contains("2022_2")) {
-        $binreturn = $bin2022_2
-    }
-    if ($pcversion.contains("2023_1")) {
-        $binreturn = $bin2023_1
-    }
-    if ($pcversion.contains("2024_1")) {
-        $binreturn = $bin2024_1
-    }
-    if ($pcversion.contains("2025_1")) {
-        $binreturn = $bin2025_1
+    switch -Regex ($pcversion) {
+        "2019" { $binreturn = $bin2019 }
+        "2020" { $binreturn = $bin2020 }
+        "2021" { $binreturn = $bin2021 }
+        "2022_1" { $binreturn = $bin2022_1 }
+        "2022_2" { $binreturn = $bin2022_2 }
+        "2023_1" { $binreturn = $bin2023_1 }
+        "2024_1" { $binreturn = $bin2024_1 }
+        "2025_1" { $binreturn = $bin2025_1 }
+        default { $binreturn = "" }
     }
 
     return $binreturn
@@ -138,9 +106,6 @@ function flash ([string]$pcversion, [string]$binpathfinal) {
 
 # Start here
 
-[string]$model = Get-CimInstance -Classname Win32_ComputerSystem | Select-Object SystemSKUNumber
-[string]$biosver = Get-CimInstance -Classname Win32_BIOS | Select-Object SMBIOSBIOSVersion
-
 $pcversion = "null"
 $isY13 = "false"
 $flashproceed = "false"
@@ -154,7 +119,7 @@ switch -Regex ($model) {
         switch -Regex ($biosver) {
             "0\.1\.1" { $pcversion = "2022_1"; break }
             "0\.5\.1" { $pcversion = "2022_2"; break }
-            default   { $pcversion = "2022_1" }
+            default { $pcversion = "2022_1" }
         }
     }
 
@@ -163,16 +128,12 @@ switch -Regex ($model) {
     "Y13G202S4EI" { $pcversion = "2025_1" }
 }
 
-clear
+Clear-Host
 header
 
 if ($pcversion.contains("null")) {
-    Write-Host "This PC: (Save this info in case of problem)"
-    Write-Host "- SKU: $($model)"
-    Write-Host "- BIOS Version: $($biosver)"
-    Write-Host ""
     Write-Host "Are you sure that PC is an Unowhy Y13 ?"
-    $confY13 = Read-Host "[Y]/[N]"
+    $confY13 = Read-Host "[Y]/[N]:"
     Write-Host ""
     if ($confY13 -eq 'y') {
         $isY13 = "true"
@@ -180,12 +141,8 @@ if ($pcversion.contains("null")) {
     }
 }
 else {
-    Write-Host "This PC: (Save this info in case of problem)"
-    Write-Host "- SKU: $($model)"
-    Write-Host "- BIOS Version: $($biosver)"
-    Write-Host ""
     Write-Host "Are you sure that Unowhy Y13 is a $(model2label($pcversion)) ?"
-    $confpcver = Read-Host "[Y]/[N]"
+    $confpcver = Read-Host "[Y]/[N]:"
     Write-Host ""
     if ($confpcver -eq 'n') {
         $isY13 = "true"
@@ -200,14 +157,14 @@ if ($pcversion.contains("null")) {
     $isY13 = "false"
 }
 
-clear
+Clear-Host
 header
 
 if ($isY13.Contains("true")) {
     Write-Host "You are ready to flash (Unowhy Y13 $(model2label($pcversion)))"
     Write-Host "Do you want to proceed now ?" -ForegroundColor Green
     Write-Host "Warning: Please, put PLUG your charger, CLOSE your programs, SAVE your works and DON'T CLOSE this window OR SHUTDOWN your PC, until the flash stop !!!" -ForegroundColor Red
-    $conf = Read-Host "[Y]/[N]"
+    $conf = Read-Host "[Y]/[N]:"
     Write-Host ""
     if ($conf -eq 'y') {
         $flashproceed = "true"
@@ -218,15 +175,19 @@ $binfile = binselect($pcversion)
 $binpath = "..\ROM\" + $binfile
 $binfinal = """$binpath"""
 
-clear
+Clear-Host
 header
 
 if ($flashproceed.Contains("true")) {
     Write-Host "Flashing..." -ForegroundColor Red
     Write-Host "File located at: $($binfinal)"
+    Write-Host ""
     flash $pcversion $binfinal
     Write-Host ""
-    Write-Host "Please Reboot, To access to the BIOS, press DEL or ESC at boot"
-    pause
-    exit
+    Write-Host "Flash done !" -ForegroundColor Green
+    Write-Host ""
+}
+
+if ($flashproceed.Contains("false")) {
+    Write-Host "Flash cancelled." -ForegroundColor Yellow
 }
